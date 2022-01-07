13df453c45c09cf376c5c5e5f7e4c239d56d3e78
anna-boser <46419226+anna-boser@users.noreply.github.com>
2022-01-06 21:20:25 -0800

    _

--- code/2_build_dataset/1_intermediate/6.75_PET_grouped_avg.R
+++ code/2_build_dataset/1_intermediate/6.75_PET_grouped_avg.R
@@ -14,27 +14,39 @@ PET <- brick(here("data", "intermediate", "PET", "PET_yeargrouped_avg.tif"))
 
 # group it by month group and avg
 
-m <- mean(PET[[c(1, 6)]])
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_0.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m0 <- mean(PET[[c(1, 6)]])
+writeRaster(m0, here("data", "intermediate", "PET", "PET_grouped_0.tif"), "GTiff", overwrite=TRUE)
+rm(m0)
 
-m <- PET[[7]]
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_1.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m1 <- PET[[7]]
+writeRaster(m1, here("data", "intermediate", "PET", "PET_grouped_1.tif"), "GTiff", overwrite=TRUE)
+rm(m1)
 
-m <- mean(PET[[c(2, 8)]])
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_2.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m2 <- mean(PET[[c(2, 8)]])
+writeRaster(m2, here("data", "intermediate", "PET", "PET_grouped_2.tif"), "GTiff", overwrite=TRUE)
+rm(m2)
 
-m <- mean(PET[[c(3, 9)]])
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_3.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m3 <- mean(PET[[c(3, 9)]])
+writeRaster(m3, here("data", "intermediate", "PET", "PET_grouped_3.tif"), "GTiff", overwrite=TRUE)
+rm(m3)
 
-m <- mean(PET[[c(4, 10)]])
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_4.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m4 <- mean(PET[[c(4, 10)]])
+writeRaster(m4, here("data", "intermediate", "PET", "PET_grouped_4.tif"), "GTiff", overwrite=TRUE)
+rm(m4)
 
-m <- mean(PET[[c(5, 11)]])
-writeRaster(m, here("data", "intermediate", "PET", "PET_grouped_5.tif"), "GTiff", overwrite=TRUE)
-rm(m)
+m5 <- mean(PET[[c(5, 11)]])
+writeRaster(m5, here("data", "intermediate", "PET", "PET_grouped_5.tif"), "GTiff", overwrite=TRUE)
+rm(m5)
+
+# m0 <- raster(here("data", "intermediate", "PET", "PET_grouped_0.tif"))
+# m1 <- raster(here("data", "intermediate", "PET", "PET_grouped_1.tif"))
+# m2 <- raster(here("data", "intermediate", "PET", "PET_grouped_2.tif"))
+# m3 <- raster(here("data", "intermediate", "PET", "PET_grouped_3.tif"))
+# m4 <- raster(here("data", "intermediate", "PET", "PET_grouped_4.tif"))
+# m5 <- raster(here("data", "intermediate", "PET", "PET_grouped_5.tif"))
+
+brick <- brick(list(m0,m1,m2,m3,m4,m5))
+writeRaster(brick, here("data", "intermediate", "PET", "PET_grouped_brick.tif"), "GTiff", overwrite=TRUE)
+mean <- mean(brick)
+writeRaster(mean, here("data", "intermediate", "PET", "PET_grouped_average.tif"), "GTiff", overwrite=TRUE)
 
