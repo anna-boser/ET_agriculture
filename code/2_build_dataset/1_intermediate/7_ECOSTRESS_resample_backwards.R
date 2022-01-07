d53ed25ed0afb248eaa19df0f4a27fc4b7254766
anna-boser <46419226+anna-boser@users.noreply.github.com>
2022-01-07 07:08:55 -0800

    _

--- code/2_build_dataset/1_intermediate/7_ECOSTRESS_resample_backwards.R
+++ code/2_build_dataset/1_intermediate/7_ECOSTRESS_resample_backwards.R
@@ -81,7 +81,7 @@ dir.create(here("data", "intermediate", "ECOSTRESS", "ET"))
 dir.create(here("data", "intermediate", "ECOSTRESS", "ET_sd"))
 
 # for loop so you can keep track of when it blew up
-for (i in 617:1){
+for (i in 541:1){
   print(paste("On timestamp number", i))
   time <- Sys.time()
   process(unique_timestamps[i])
