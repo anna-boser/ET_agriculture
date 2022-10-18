ce8dc2202952ce268e19dab8fa7bd4e7a82e0ea9
anna-boser <46419226+anna-boser@users.noreply.github.com>
2022-10-17 17:05:09 -0700

    tidy up the tidy dataset script and rerun for with water

--- code/2_build_dataset/2_for_analysis/1.5_tidy_dataset_cv.R
+++ code/2_build_dataset/2_for_analysis/1.5_tidy_dataset_cv.R
@@ -18,8 +18,8 @@ tidy <- function(data){
   data$cpad <- NULL
   data$cpad_fveg <- NULL
   data$cdl_fveg <- NULL
-  ET <- data[,13:18]
-  data <- data[,1:12]
+  ET <- data[,17:18]
+  data <- data[,1:16]
   clean <- pivot_longer(data, cols = paste0("PET", 0:5), names_to = "monthgroup", names_prefix = "PET", values_to = "PET")
   ET <- pivot_longer(ET, cols = paste0("ET", 0:5), names_to = "monthgroup", names_prefix = "ET", values_to = "ET")
   clean$ET <- ET$ET
@@ -35,114 +35,15 @@ to_mm <- function(watts_m2, date){ #watts = J/s
   kg = watts_m2*hours_of_daylight*3600*(1/2257)*(1/1000) # J/s * hours (h) * seconds in an hour (s/h) * latent heat of fusion (g/J) * conversion to kg AKA mm over a m2 (kg/g)
 }
 
-# dates picked to minimize problems with Jensen's inequality: 
-# 2: June 1
-# 3: August 15
-# 4: October 15
-
-# tidy ag
-data <- fread(here("data", "for_analysis", "agriculture_not_tidy_cv.csv"))
-data <- tidy(data)
-fwrite(data, here("data", "for_analysis", "agriculture_cv.csv"))
-
-# only growing season
-data <- dplyr::filter(data, monthgroup %in% 2:4)
-data$agriculture <- NULL
-data$counterfactual <- NULL
-fwrite(data, here("data", "for_analysis", "agriculture_cv_gs.csv"))
-
-# change ET units to mm and remove NA data
-data <- filter(data, ET>=0)
-data$ET <- ifelse(data$monthgroup == 2, to_mm(data$ET, as.Date("2019/06/01")), data$ET)
-data$ET <- ifelse(data$monthgroup == 3, to_mm(data$ET, as.Date("2019/08/15")), data$ET)
-data$ET <- ifelse(data$monthgroup == 4, to_mm(data$ET, as.Date("2019/10/15")), data$ET)
-fwrite(data, here("data", "for_analysis", "agriculture_cv_gs_mm.csv"))
-
-#tidy counterfactual
-data <- fread(here("data", "for_analysis", "counterfactual_not_tidy_cv.csv"))
-data <- tidy(data)
-fwrite(data, here("data", "for_analysis", "counterfactual_cv.csv"))
-
-# only growing season
-data <- dplyr::filter(data, monthgroup %in% 2:4)
-data$agriculture <- NULL
-data$counterfactual <- NULL
-fwrite(data, here("data", "for_analysis", "counterfactual_cv_gs.csv"))
-
-# change ET units to mm
-data <- filter(data, ET>=0)
-data$ET <- ifelse(data$monthgroup == 2, to_mm(data$ET, as.Date("2019/06/01")), data$ET)
-data$ET <- ifelse(data$monthgroup == 3, to_mm(data$ET, as.Date("2019/08/15")), data$ET)
-data$ET <- ifelse(data$monthgroup == 4, to_mm(data$ET, as.Date("2019/10/15")), data$ET)
-fwrite(data, here("data", "for_analysis", "counterfactual_cv_gs_mm.csv"))
-
-####################################################################################
-# After visually inspecting the simulated counterfactual using the above dataset, 
-# it became clear that certain locations were likely contaminated and were irrigated. 
-# Therefore, here we screen for locations that are likely not natural land and get rid of them. 
-
-data <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
-
-# remove monthgroups
-data <- pivot_wider(data, names_from = c(monthgroup), values_from = c(ET, PET))
-
-data$ET <- rowMeans(select(data, ET_2, ET_3, ET_4), na.rm = FALSE)
-data$PET <- rowMeans(select(data, PET_2, PET_3, PET_4), na.rm = FALSE)
-
-data <- data %>% select(-ET_2, -ET_3, -ET_4, -PET_2, -PET_3, -PET_4)
-
-# remove any NA values
-data <- filter(data, !(is.na(ET)))
-
-# inspect <- filter(data, ET>2.5)
-
-# library(ggplot2)
-# inspect %>%
-#   ggplot() +
-#   geom_point(aes(x = x, y = y, color = ET), size = .1) +
-#   scale_color_gradientn(name="ET (mm/day)", colours = c("darkgoldenrod4", "darkgoldenrod2", "khaki1", "lightgreen", "turquoise3", "deepskyblue3", "mediumblue", "navyblue", "midnightblue", "black")) +
-#   theme_void()
-
-# After visual inspection, anything above 4 looks like it could plausibly be irrigated. 
-# This represents .1% of the data, so we feel that it is unlikely that we are biasing the natural counterfactual downward. 
-
-# identify these locations and remove them from the dataset
-loc <- paste(filter(data, ET < 4)$x, filter(data, ET < 4)$y)
-
-data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
-data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
-
-fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<4.csv"))
-
-# anything above 4.5; .03%
-loc <- paste(filter(data, ET < 4.5)$x, filter(data, ET < 4.5)$y)
-
-data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
-data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
-
-fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<4.5.csv"))
-
-# anything above 5; .005% of the data
-loc <- paste(filter(data, ET < 5)$x, filter(data, ET < 5)$y)
-
-data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
-data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
-
-fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<5.csv"))
-
-####################################################################################
-# After this, I decided to look into some other possible counterfactual datasets. 
-# here, I tidy those as well
-
 big_tide <- function(inloc, outloc, outloc_gs_mm){
   #tidy counterfactual
   data <- fread(inloc)
   data <- tidy(data)
   fwrite(data, outloc)
-
+  
   # only growing season
   data <- dplyr::filter(data, monthgroup %in% 2:4)
-
+  
   # change ET units to mm
   data <- filter(data, ET>=0)
   data$ET <- ifelse(data$monthgroup == 2, to_mm(data$ET, as.Date("2019/06/01")), data$ET)
@@ -151,26 +52,98 @@ big_tide <- function(inloc, outloc, outloc_gs_mm){
   fwrite(data, outloc_gs_mm)
 }
 
+# dates picked to minimize problems with Jensen's inequality: 
+# 2: June 1
+# 3: August 15
+# 4: October 15
+
+# ag
+big_tide(inloc = here("data", "for_analysis", "agriculture_not_tidy_cv.csv"), 
+         outloc = here("data", "for_analysis", "agriculture_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "agriculture_cv_gs_mm.csv")
+)
+
+# cdl (nlcd) (original "counterfactual")
+big_tide(inloc = here("data", "for_analysis", "counterfactual_not_tidy_cv.csv"), 
+         outloc = here("data", "for_analysis", "counterfactual_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "counterfactual_cv_gs_mm.csv")
+)
+
 #fveg
 big_tide(inloc = here("data", "for_analysis", "fveg_not_tidy_cv.csv"), 
-outloc = here("data", "for_analysis", "fveg_cv.csv"), 
-outloc_gs_mm = here("data", "for_analysis", "fveg_cv_gs_mm.csv")
+         outloc = here("data", "for_analysis", "fveg_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "fveg_cv_gs_mm.csv")
 )
 
 #cpad
 big_tide(inloc = here("data", "for_analysis", "cpad_not_tidy_cv.csv"), 
-outloc = here("data", "for_analysis", "cpad_cv.csv"), 
-outloc_gs_mm = here("data", "for_analysis", "cpad_cv_gs_mm.csv")
+         outloc = here("data", "for_analysis", "cpad_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "cpad_cv_gs_mm.csv")
 )
 
 #cpad_fveg
 big_tide(inloc = here("data", "for_analysis", "cpad_fveg_not_tidy_cv.csv"), 
-outloc = here("data", "for_analysis", "cpad_fveg_cv.csv"), 
-outloc_gs_mm = here("data", "for_analysis", "cpad_fveg_cv_gs_mm.csv")
+         outloc = here("data", "for_analysis", "cpad_fveg_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "cpad_fveg_cv_gs_mm.csv")
 )
 
 #cdl_fveg
 big_tide(inloc = here("data", "for_analysis", "cdl_fveg_not_tidy_cv.csv"), 
-outloc = here("data", "for_analysis", "cdl_fveg_cv.csv"), 
-outloc_gs_mm = here("data", "for_analysis", "cdl_fveg_cv_gs_mm.csv")
+         outloc = here("data", "for_analysis", "cdl_fveg_cv.csv"), 
+         outloc_gs_mm = here("data", "for_analysis", "cdl_fveg_cv_gs_mm.csv")
 )
+
+# ####################################################################################
+# # This code is to remove outliers. 
+# # After visually inspecting the simulated counterfactual (cdl) using the above dataset, 
+# # it became clear that certain locations were likely contaminated and were irrigated. 
+# # Therefore, here we screen for locations that are likely not natural land and get rid of them. 
+# 
+# data <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
+# 
+# # remove monthgroups
+# data <- pivot_wider(data, names_from = c(monthgroup), values_from = c(ET, PET))
+# 
+# data$ET <- rowMeans(select(data, ET_2, ET_3, ET_4), na.rm = FALSE)
+# data$PET <- rowMeans(select(data, PET_2, PET_3, PET_4), na.rm = FALSE)
+# 
+# data <- data %>% select(-ET_2, -ET_3, -ET_4, -PET_2, -PET_3, -PET_4)
+# 
+# # remove any NA values
+# data <- filter(data, !(is.na(ET)))
+# 
+# # inspect <- filter(data, ET>2.5)
+# 
+# # library(ggplot2)
+# # inspect %>%
+# #   ggplot() +
+# #   geom_point(aes(x = x, y = y, color = ET), size = .1) +
+# #   scale_color_gradientn(name="ET (mm/day)", colours = c("darkgoldenrod4", "darkgoldenrod2", "khaki1", "lightgreen", "turquoise3", "deepskyblue3", "mediumblue", "navyblue", "midnightblue", "black")) +
+# #   theme_void()
+# 
+# # After visual inspection, anything above 4 looks like it could plausibly be irrigated. 
+# # This represents .1% of the data, so we feel that it is unlikely that we are biasing the natural counterfactual downward. 
+# 
+# # identify these locations and remove them from the dataset
+# loc <- paste(filter(data, ET < 4)$x, filter(data, ET < 4)$y)
+# 
+# data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
+# data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
+# 
+# fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<4.csv"))
+# 
+# # anything above 4.5; .03%
+# loc <- paste(filter(data, ET < 4.5)$x, filter(data, ET < 4.5)$y)
+# 
+# data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
+# data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
+# 
+# fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<4.5.csv"))
+# 
+# # anything above 5; .005% of the data
+# loc <- paste(filter(data, ET < 5)$x, filter(data, ET < 5)$y)
+# 
+# data2 <- fread(here("data/for_analysis/counterfactual_cv_gs_mm.csv"))
+# data2 <- filter(data2, paste(data2$x, data2$y) %in% loc)
+# 
+# fwrite(data2, here("data", "for_analysis", "counterfactual_cv_gs_mm<5.csv"))
